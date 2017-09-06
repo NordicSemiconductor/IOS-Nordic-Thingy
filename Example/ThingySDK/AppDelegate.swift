2ea01d026762fb4a566c7020b06d46597ab65d0f
Berg, Mostafa <mostafa.berg@nordicsemi.no>
2017-09-06 09:14:28 +0200

    +Updated bundled firmware to 2.0.0 +Bumped current firmware version enum to 2.0.0

--- Example/ThingySDK/AppDelegate.swift
+++ Example/ThingySDK/AppDelegate.swift
@@ -47,7 +47,7 @@ import IOSThingyLibrary
 
 let kViewedMenuTooltip      = "viewed_menu_tooltip"
 let kViewedSensorsTooltip   = "viewed_sensors_tooltip"
-let kCurrentDfuVersion      = "1.1.0"
+let kCurrentDfuVersion      = "2.0.0"
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
