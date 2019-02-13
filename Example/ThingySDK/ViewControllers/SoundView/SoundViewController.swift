5f663c3664173ec158704bfc3d64650b752a6743
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2019-02-13 16:20:36 +0100

    AVAudioSession fix and workaround

--- Example/ThingySDK/ViewControllers/SoundView/SoundViewController.swift
+++ Example/ThingySDK/ViewControllers/SoundView/SoundViewController.swift
@@ -177,7 +177,13 @@ class SoundViewController: SwipableTableViewController {
         volumeControl.addGestureRecognizer(volumeSliderTapRecognizer)
         recordingSession = AVAudioSession.sharedInstance()
         do {
-            try recordingSession.setCategory(convertFromAVAudioSessionCategory(AVAudioSession.Category.playAndRecord))
+            if #available(iOS 10.0, *) {
+                try recordingSession.setCategory(.playAndRecord, mode: .default)
+            } else {
+                // Workaround until https://forums.swift.org/t/using-methods-marked-unavailable-in-swift-4-2/14949 isn't fixed
+                // See: https://stackoverflow.com/a/52396244/2115352
+                recordingSession.perform(NSSelectorFromString("setCategory:error:"), with: AVAudioSession.Category.playAndRecord)
+            }
             try recordingSession.setPreferredSampleRate(8000) // 8 kHz
             try recordingSession.setPreferredOutputNumberOfChannels(1)
             try recordingSession.setActive(true)
@@ -557,9 +563,4 @@ class SoundViewController: SwipableTableViewController {
         player = nil
         engine = nil
     }
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromAVAudioSessionCategory(_ input: AVAudioSession.Category) -> String {
-	return input.rawValue
 }
